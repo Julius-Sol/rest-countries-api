064394f882a5a620147b007ea4b61cf458adce37
Julio S <85770548+Julius-Sol@users.noreply.github.com>
2023-06-19 19:24:49 -0400

    Add dark mode toggle

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,16 @@ import { useState,useEffect } from 'react'
 import axios from 'axios';
 import './App.css'
 import CountryCard from './CountryCard';
+import Navigation from './Navigation';
 
 
 function App() {
   const [countryArray, setCountryArray] = useState([]);
+  const [darkMode, setDarkMode] = useState(false);
+
+  const ToggleDarkMode = ()=>{
+    setDarkMode((prevElement)=>{return !prevElement});
+  }
 
   useEffect(()=>{
     axios.get('https://restcountries.com/v3.1/all')
@@ -25,7 +31,8 @@ function App() {
   },[])
 
   return (
-    <main className=''>
+    <main className={darkMode ? 'dark': ''}>
+      <Navigation toggleDark={ToggleDarkMode}></Navigation>
       <section className='bg-LightBackground font-nunito-sans dark:bg-DarkBackground '>
         <CountryCard countryArray={countryArray}></CountryCard>
       </section>
---
+++ src/Navigation.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import moon from './assets/moon-outline.svg'
+
+function Navigation({toggleDark}) {
+
+  return (
+    <nav className=' bg-white py-6 dark:bg-DarkElement'>
+      <div className='flex flex-row justify-between m-auto w-[90%] items-center dark:text-white'>
+        <h1 className='font-semibold'>Where in the World?</h1>
+        <div className='flex flex-row gap-3 items-center justify-center text-xs font-semibold hover:cursor-pointer' onClick={()=>{toggleDark()}}>
+          <img src={moon} alt="moon" className='w-[16px] filter dark:invert'/>
+          <p className=''>Dark Mode</p>
+        </div>
+      </div>  
+    </nav>
+  )
+}
+
+export default Navigation;
