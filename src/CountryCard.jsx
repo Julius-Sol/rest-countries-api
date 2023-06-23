6d8ca3b790b447fc6980302c258255b7c478bb07
Julio S <85770548+Julius-Sol@users.noreply.github.com>
2023-06-23 17:08:54 -0400

    Add functionality to country card when clicked - Refactor mapping all country array to be more reusable - Add state variable to hide all countries - Add ternery to show either back button or all countries - Add function to handle when country card is clicked

--- src/CountryCard.jsx
+++ src/CountryCard.jsx
@@ -1,17 +1,52 @@
 import { useState } from 'react';
 import searchIcon from './assets/search-outline.svg';
 import chevronIcon from './assets/chevron-down-outline.svg';
-
+import backArrow from './assets/arrow-back-outline.svg';
  
   const CountryCard = ({countryArray}) =>{
     const [search, setSearch] = useState('');
     const [filter, setFilter] = useState('');
     const [hideDrop, setHideDrop] = useState(true);
-    const searchFailText = <p className='dark:text-white text-3xl mt-[50%]'>No Search Found</p> ;
+    const [hideAll, setHideAll] = useState(false);
+    const searchFailText = <p className='dark:text-white text-3xl mt-5'>No Search Found</p> ;
+    let country = [] ;
 
+    //Function to change hideAll state to False.
+      const UnHideAll = () =>{
+        setHideAll(()=>true);
+      } 
+
+      const HideAll = () =>{
+        setHideAll(()=>false);
+      } 
+
+    //Function to handle when country card is clicked.
+    const CountryClick = (event)=> {
+      let countryTitle = event.currentTarget.children[1].children[0].textContent.toLowerCase();
+      let countryInfo = countryArray.filter((element)=>element.name.common.toLowerCase().includes(countryTitle));
+      console.log(countryInfo);
+      UnHideAll();
+    }
+
+    // Function to unhide the filter dropmenue
     const unhideDrop = () => {
       setHideDrop(()=>false)
     }
+
+      // Maps countries array to display
+   const CardFactory = (array) => { return (array.map((country,index)=>{
+    return(
+      <div key={index} className='mt-5 mb-5 md:max-w-[250px] md:max-h-[250px]' onClick={CountryClick}>
+        <img src={country.flags.png} alt={country.flags.alt} className='w-full md:h-[150px] md:w-[300px]'/>
+        <div className='bg-white px-5 pb-10 pt-5 shadow-[0_1px_3px_0px_rgba(133,133,133,0.2)] dark:bg-DarkElement dark:text-white max-h-[150px]'>
+          <h1 className='font-bold mb-3'>{country.name.common}</h1>
+          <p className='text-[.8rem]'><span className='font-semibold '>Population:</span> {country.population.toLocaleString("en-US")}</p>
+          <p className='text-[.8rem]'><span className='font-semibold'>Region:</span> {country.region}</p>
+          <p className='text-[.8rem]'><span className='font-semibold'>Captial:</span> {country.capital}</p>
+        </div>
+      </div>
+    )
+  }))}
 
   //Function to handle state change of search input
     const handleChange = (event) =>{
@@ -35,25 +70,20 @@ import chevronIcon from './assets/chevron-down-outline.svg';
   // Variable to store array of countries. Filters countries array to find search query. If there
   // is no query then it will show all countries avalible
   const countries = search ?  filteredCountries.filter((el) => el.name.common.toLowerCase().includes(search.toLowerCase())) : filteredCountries;  
-
-  // Maps countries array to display
-  const country = countries.map((country,index)=>{
-    return(
-      <div key={index} className='mt-5 mb-5 md:max-w-[250px] md:max-h-[250px]'>
-        <img src={country.flags.png} alt={country.flags.alt} className='w-full md:h-[150px] md:w-[300px]'/>
-        <div className='bg-white px-5 pb-10 pt-5 shadow-[0_1px_3px_0px_rgba(133,133,133,0.2)] dark:bg-DarkElement dark:text-white max-h-[150px]'>
-          <h1 className='font-bold mb-3'>{country.name.common}</h1>
-          <p className='text-[.8rem]'><span className='font-semibold '>Population:</span> {country.population.toLocaleString("en-US")}</p>
-          <p className='text-[.8rem]'><span className='font-semibold'>Region:</span> {country.region}</p>
-          <p className='text-[.8rem]'><span className='font-semibold'>Captial:</span> {country.capital}</p>
-        </div>
-      </div>
-    )
-  })
-  
+  country = CardFactory(countries);
 
   return(
     <div className='pt-6'>
+      {hideAll ? 
+      <div className='m-auto w-[90%]'>
+        <div className='flex gap-2 bg-white dark:bg-DarkElement px-4 py-2 w-24 text-sm shadow-[0_2px_4px_1px_rgba(133,133,133,0.2)] hover:cursor-pointer' onClick={HideAll}>
+          <img src={backArrow} alt="back-arrow" className='w-5 dark:invert' />
+          <p className='dark:text-white' >Back</p>
+        </div>
+      </div> 
+
+      :  
+      
       <div className='flex flex-col md:flex-row md:justify-between items-center m-auto w-[90%] px-4'>
         {/* Search input */}
         <label htmlFor="search" className='flex bg-white px-5 py-4 m-auto w-[90%] shadow-[0_1px_3px_0px_rgba(133,133,133,0.2)] rounded dark:bg-DarkElement dark:text-white md:w-[30rem] md:h-[100%] md:m-0 md:p-2 md:py-4'>
@@ -79,10 +109,10 @@ import chevronIcon from './assets/chevron-down-outline.svg';
               <button className='text-left w-48 bg-white dark:bg-DarkElement dark:text-white px-4 py-1 text-sm' value='oceania' onClick={handleFilterClick}>Oceania</button>
             </div>
           </div>
-      </div>
+      </div>}
         
-      <div className='flex flex-col m-auto w-[70%] justify-center items-center md:flex-row md:flex-wrap md:gap-16 md:w-[100%]'>
-        {country <= 0 ? searchFailText : country }
+      <div className='flex flex-col m-auto w-[70%] justify-center items-center md:flex-row md:flex-wrap md:gap-16 md:w-[100%]' >
+        {country <= 0 ? searchFailText : (hideAll?  '' : country) }
       </div>
     </div>
   )
